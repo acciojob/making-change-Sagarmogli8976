71b23d3b38d4e9c30c5de3939e168f3a1b64975b
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-03-28 18:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	c = Number(c); // convert input to number
+
+  const q = Math.floor(c / 25);
+  c = c % 25;
+
+  const d = Math.floor(c / 10);
+  c = c % 10;
+
+  const n = Math.floor(c / 5);
+  c = c % 5;
+
+  const p = c;
+
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
